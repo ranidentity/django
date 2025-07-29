7ece27d5b169a4ee9ed51fc42892a4e42fd51ab7
ranidentity <chiewwei1991@gmail.com>
2025-07-29 18:00:11 +0800

    init

---
+++ my_ai_portal/members/views.py
@@ -0,0 +1,9 @@
+from django.template import loader
+from django.http import HttpResponse
+from .models import Member
+
+# Create your views here.
+def members(request):
+    mymember = Member.object.all().values()
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
